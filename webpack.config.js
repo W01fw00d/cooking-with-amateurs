dc7f551d27375ed353204e8214b96cade9534c29
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-04-28 12:15:54 +0900

    Fix public path in devServer and prod

--- webpack.config.js
+++ webpack.config.js
@@ -35,7 +35,7 @@ module.exports = {
     filename: 'bundle.js',
   },
   devServer: {
-    contentBase: path.join(__dirname, 'public/'),
+    contentBase: __dirname,
     port: 3000,
     publicPath: 'http://localhost:3000/dist/',
     hotOnly: true,
