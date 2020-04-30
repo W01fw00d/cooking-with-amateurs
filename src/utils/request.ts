c01c525e8981df43a0150c925b137e79b9d9e7c9
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-04-30 16:12:54 +0900

    Put request.ts in manual prod mode; make tsconfig more strict

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -1,8 +1,8 @@
 const httpGet = (params: string, callback: Function) => {
   // prod
-  // const url = `/${params}`;
+  const url = `/${params}`;
   // dev
-  const url = `http://localhost:5000/${params}`;
+  // const url = `http://localhost:5000/${params}`;
   const xmlHttp = new XMLHttpRequest();
   xmlHttp.onreadystatechange = () => {
     if (xmlHttp.readyState === 4 && xmlHttp.status === 200) {
