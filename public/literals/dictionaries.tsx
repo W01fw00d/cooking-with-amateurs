7a55d299bdcab3dea6cbb7bca0cbf3d186a266e8
Gabriel Romay Machado <romay.gabriel@gmail.com>
2020-11-07 22:49:26 +0900

    Add new translated steps to dictionaries

--- public/literals/dictionaries.tsx
+++ public/literals/dictionaries.tsx
@@ -5,12 +5,25 @@ import ingredientsEn from './english/ingredients.json';
 import ingredientsSectionsEn from './english/ingredientsSections.json';
 import recipesNamesEn from './english/recipesNames.json';
 
+import coulantEn from './english/steps/coulant.json';
+import croquettesEn from './english/steps/croquettes.json';
+import custardEn from './english/steps/custard.json';
+import fideuaEn from './english/steps/fideua.json';
+import gnocchiEn from './english/steps/gnocchi.json';
+import gyozasEn from './english/steps/gyozas.json';
+import meatballsEn from './english/steps/meatballs.json';
+import butifarraRagoutEn from './english/steps/butifarraRagout.json';
+import risottoEn from './english/steps/risotto.json';
+import salmonTartarEn from './english/steps/salmonTartar.json';
+
+
 import commonJp from './japanese/common.json';
 import recipeJp from './japanese/recipe.json';
 import ingredientsJp from './japanese/ingredients.json';
 import projectDataJp from './japanese/projectData.json';
 import recipesNamesJp from './japanese/recipesNames.json';
 import ingredientsSectionsJp from './japanese/ingredientsSections.json';
+
 
 import commonMa from './majorcan/common.json';
 import ingredientsMa from './majorcan/ingredients.json';
@@ -19,6 +32,18 @@ import projectDataMa from './majorcan/projectData.json';
 import recipeMa from './majorcan/recipe.json';
 import recipesNamesMa from './majorcan/recipesNames.json';
 
+import coulantMa from './majorcan/steps/coulant.json';
+import croquettesMa from './majorcan/steps/croquettes.json';
+import custardMa from './majorcan/steps/custard.json';
+import fideuaMa from './majorcan/steps/fideua.json';
+import gnocchiMa from './majorcan/steps/gnocchi.json';
+import gyozasMa from './majorcan/steps/gyozas.json';
+import meatballsMa from './majorcan/steps/meatballs.json';
+import butifarraRagoutMa from './majorcan/steps/butifarraRagout.json';
+import risottoMa from './majorcan/steps/risotto.json';
+import salmonTartarMa from './majorcan/steps/salmonTartar.json';
+
+
 import commonSp from './spanish/common.json';
 import recipeSp from './spanish/recipe.json';
 import projectDataSp from './spanish/projectData.json';
@@ -26,16 +51,16 @@ import ingredientsSp from './spanish/ingredients.json';
 import ingredientsSectionsSp from './spanish/ingredientsSections.json';
 import recipesNamesSp from './spanish/recipesNames.json';
 
-import coulant from './spanish/steps/coulant.json';
-import croquettes from './spanish/steps/croquettes.json';
-import custard from './spanish/steps/custard.json';
-import fideua from './spanish/steps/fideua.json';
-import gnocchi from './spanish/steps/gnocchi.json';
-import gyozas from './spanish/steps/gyozas.json';
-import meatballs from './spanish/steps/meatballs.json';
-import butifarraRagout from './spanish/steps/butifarraRagout.json';
-import risotto from './spanish/steps/risotto.json';
-import salmonTartar from './spanish/steps/salmonTartar.json';
+import coulantSp from './spanish/steps/coulant.json';
+import croquettesSp from './spanish/steps/croquettes.json';
+import custardSp from './spanish/steps/custard.json';
+import fideuaSp from './spanish/steps/fideua.json';
+import gnocchiSp from './spanish/steps/gnocchi.json';
+import gyozasSp from './spanish/steps/gyozas.json';
+import meatballsSp from './spanish/steps/meatballs.json';
+import butifarraRagoutSp from './spanish/steps/butifarraRagout.json';
+import risottoSp from './spanish/steps/risotto.json';
+import salmonTartarSp from './spanish/steps/salmonTartar.json';
 
 export default {
   en: {
@@ -45,18 +70,17 @@ export default {
     projectData: projectDataEn,
     recipe: recipeEn,
     recipesNames: recipesNamesEn,
-    //TODO: create translated files for steps
     recipeSteps: {
-      coulant,
-      croquettes,
-      custard,
-      fideua,
-      gnocchi,
-      gyozas,
-      meatballs,
-      butifarraRagout,
-      risotto,
-      salmonTartar,
+      coulant: coulantEn,
+      croquettes: croquettesEn,
+      custard: custardEn,
+      fideua: fideuaEn,
+      gnocchi: gnocchiEn,
+      gyozas: gyozasEn,
+      meatballs: meatballsEn,
+      butifarraRagout: butifarraRagoutEn,
+      risotto: risottoEn,
+      salmonTartar: salmonTartarEn,
     },
   },
   jp: {
@@ -68,16 +92,16 @@ export default {
     recipesNames: recipesNamesJp,
     //TODO: create translated files for steps
     recipeSteps: {
-      coulant,
-      croquettes,
-      custard,
-      fideua,
-      gnocchi,
-      gyozas,
-      meatballs,
-      butifarraRagout,
-      risotto,
-      salmonTartar,
+      coulant: coulantEn,
+      croquettes: croquettesEn,
+      custard: custardEn,
+      fideua: fideuaEn,
+      gnocchi: gnocchiEn,
+      gyozas: gyozasEn,
+      meatballs: meatballsEn,
+      butifarraRagout: butifarraRagoutEn,
+      risotto: risottoEn,
+      salmonTartar: salmonTartarEn,
     },
   },
   ma: {
@@ -87,18 +111,17 @@ export default {
     projectData: projectDataMa,
     recipe: recipeMa,
     recipesNames: recipesNamesMa,
-    //TODO: create translated files for steps
     recipeSteps: {
-      coulant,
-      croquettes,
-      custard,
-      fideua,
-      gnocchi,
-      gyozas,
-      meatballs,
-      butifarraRagout,
-      risotto,
-      salmonTartar,
+      coulant: coulantMa,
+      croquettes: croquettesMa,
+      custard: custardMa,
+      fideua: fideuaMa,
+      gnocchi: gnocchiMa,
+      gyozas: gyozasMa,
+      meatballs: meatballsMa,
+      butifarraRagout: butifarraRagoutMa,
+      risotto: risottoMa,
+      salmonTartar: salmonTartarMa,
     },
   },
   sp: {
@@ -109,16 +132,16 @@ export default {
     recipe: recipeSp,
     recipesNames: recipesNamesSp,
     recipeSteps: {
-      coulant,
-      croquettes,
-      custard,
-      fideua,
-      gnocchi,
-      gyozas,
-      meatballs,
-      butifarraRagout,
-      risotto,
-      salmonTartar,
+      coulant: coulantSp,
+      croquettes: croquettesSp,
+      custard: custardSp,
+      fideua: fideuaSp,
+      gnocchi: gnocchiSp,
+      gyozas: gyozasSp,
+      meatballs: meatballsSp,
+      butifarraRagout: butifarraRagoutSp,
+      risotto: risottoSp,
+      salmonTartar: salmonTartarSp,
     },
   },
 };
