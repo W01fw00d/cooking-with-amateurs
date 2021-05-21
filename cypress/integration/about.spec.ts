f1b4fc2bf6525e1675a5e39c9dd90a5f9cac304b
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-05-21 16:20:04 +0900

    Fix about.spec; code auto format

--- cypress/integration/about.spec.ts
+++ cypress/integration/about.spec.ts
@@ -1,14 +1,40 @@
-describe('On About Page', function () {
+describe('On About Page', () => {
   const URL = 'about';
 
-  it('User sees name and email', function () {
+  const user = 'gabriel';
+
+  const setStub = () => {
+    cy.server();
+
+    cy.route({
+      method: 'GET',
+      url: '/literals/en',
+      response: {
+        template: {
+          common: {
+            gabriel: user,
+            about: 'about',
+            participants: 'participants',
+            cv: 'cv',
+          },
+          projectData: {
+            description: 'description',
+          },
+        },
+      },
+    });
+  };
+
+  it('User sees name and email', () => {
+    setStub();
+
     cy.visit(URL);
 
-    cy.contains('Gabriel Romay Machado');
+    cy.contains(user);
     cy.contains('romay.gabriel@gmail.com');
   });
 
-  it('User goes back to home', function () {
+  it('User goes back to home', () => {
     cy.visit(URL);
 
     cy.get('#back').click();
--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -4,26 +4,36 @@ import { RecipeAboutTemplate as About } from 'chemistry-ui';
 import translate from '../language/translate';
 
 export default () => {
-    const common = translate('common');
-    const projectData = translate('projectData');
+  const common = translate('common');
+  const projectData = translate('projectData');
 
-    return (
-        <About
-            literals={{
-                about: common.about,
-                participants: common.participants,
-            }}
-            authorData={{
-                name: common.gabriel,
-                email: 'romay.gabriel@gmail.com',
-                url: common.cv,
-            }}
-            projectData={{
-                description: projectData.description,
-                url: 'https://github.com/W01fw00d/cooking-with-amateurs/blob/master/README.md',
-                participants:
-                    ["Mateu", "Marina", "Jose Luis", "Iris", "Malén", "PJ", "Francina", "Lluis", "Marta", "Gabriel"],
-            }}
-        />
-    );
+  return (
+    <About
+      literals={{
+        about: common.about,
+        participants: common.participants,
+      }}
+      authorData={{
+        name: common.gabriel,
+        email: 'romay.gabriel@gmail.com',
+        url: common.cv,
+      }}
+      projectData={{
+        description: projectData.description,
+        url: 'https://github.com/W01fw00d/cooking-with-amateurs/blob/master/README.md',
+        participants: [
+          'Mateu',
+          'Marina',
+          'Jose Luis',
+          'Iris',
+          'Malén',
+          'PJ',
+          'Francina',
+          'Lluis',
+          'Marta',
+          'Gabriel',
+        ],
+      }}
+    />
+  );
 };
