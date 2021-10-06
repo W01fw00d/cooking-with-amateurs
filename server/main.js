ffe6c790dc8e5144a655f7f46d689d49eced2ce6
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-10-06 18:23:31 +0900

    Set mongoDBUrl as a node env var (#73)
    * Set mongoDBUrl as a node env var

--- server/main.js
+++ server/main.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 5000;
 const PRODUCTION_ENV = 'production';
 const isProdEnv = process.env.NODE_ENV === PRODUCTION_ENV;
 
-mongoDB.init(isProdEnv);
+mongoDB.init(process.env.MONGODB_URL);
 app.init(PORT, isProdEnv);
 
 console.log(`RESTful API server started on: ${PORT}`);
