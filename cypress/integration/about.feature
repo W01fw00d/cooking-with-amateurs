2ce826b4710faed8dd80f93b08e4d39e6a19e733
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-10-26 15:37:59 +0900

    Complete about.feature

--- cypress/integration/about.feature
+++ cypress/integration/about.feature
@@ -1,19 +1,22 @@
 Feature: On About Page
 
-  Background:
-    Given I visit "About" Page
-
   @core
   Scenario: I see the page title
+    Given System loads About Page only
+    And I visit "About" Page
     Then I see "about" in the page
 
   @core
   Scenario: See author details
+    Given System loads About Page only
+    And I visit "About" Page
     Then I see "gabriel" in the page
-    Then I see "romay.gabriel@gmail.com" in the page
+    And I see "romay.gabriel@gmail.com" in the page
 
   Scenario: Go back to Home Page
+    Given System loads About and List Page
+    And I visit "About" Page
+    Then I see "about" in the page
     When I click on the "back" Button
     Then I am redirected to "List" Page
-    # TODO: setStub for List Page, so it's not displayed blank, and check
-    # Then I see "Fideuà" in the page
+    And I see "Fideuà" in the page
--- cypress/integration/about/index.ts
+++ cypress/integration/about/index.ts
@@ -1,19 +1,44 @@
-import { Before } from 'cypress-cucumber-preprocessor/steps';
+import { Given } from 'cypress-cucumber-preprocessor/steps';
 
-import { getCommon, getProjectData } from '../../fixtures/about/literals';
+import getCommon from '../../fixtures/list/literals';
+import getRecipe from '../../fixtures/recipe';
+import { getCommon as aboutGetCommon, getProjectData } from '../../fixtures/about/literals';
 
-Before(() => {
+Given('System loads About Page only', () => {
   /* TODO: move hardcoded data in the about page to a json and stub it here,
     using random lenght array for participants
     */
   cy.intercept('GET', '/literals/en', {
     template: {
       common: {
-        ...getCommon(),
+        ...aboutGetCommon(),
         gabriel: 'gabriel',
         about: 'about',
       },
       projectData: getProjectData(),
     },
   });
+});
+
+Given('System loads About and List Page', () => {
+  cy.intercept('GET', '/literals/en', {
+    template: {
+      common: { ...getCommon(), ...aboutGetCommon(), gabriel: 'gabriel', about: 'about' },
+      projectData: getProjectData(),
+    },
+    data: {
+      recipesNames: {
+        fideua: 'Fideuà',
+      },
+    },
+  });
+
+  cy.intercept('GET', 'recipes', [
+    {
+      ...getRecipe(),
+      id: '1',
+      code: 'fideua',
+      showName: true,
+    },
+  ]);
 });
