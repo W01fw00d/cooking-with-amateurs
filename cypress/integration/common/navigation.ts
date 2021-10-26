b9f158a927d4a72f6e346703908b73c7811e8148
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-10-26 17:09:56 +0900

    Check console.error on cypress test

--- cypress/integration/common/navigation.ts
+++ cypress/integration/common/navigation.ts
@@ -1,7 +1,13 @@
 import { Given, Then } from 'cypress-cucumber-preprocessor/steps';
 
 Given('I visit {string} Page', page => {
-  cy.visit(page.toLowerCase());
+  cy.visit(page.toLowerCase(), {
+    onBeforeLoad(win) {
+      cy.stub(win.console, 'error').as('consoleError');
+      // Disabled warn check because it causes too much noise in tests
+      // cy.stub(win.console, 'warn').as('consoleWarn');
+    },
+  });
 });
 
 Then('I am redirected to {string} Page', page => {
