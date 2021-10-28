dc6136d6711ff4eb3623c11c84ea5d7469d719db
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-10-28 14:52:53 +0900

    Load languageOptions.json using API in pages/recipeList

--- server/api/routes/routes.js
+++ server/api/routes/routes.js
@@ -1,8 +1,9 @@
-module.exports = (app) => {
+module.exports = app => {
   const controller = require('../controllers/controller');
 
   app.route('/recipes').get(controller.list_all_recipes);
   app.route('/details/:recipeId').get(controller.read_a_recipe_details);
   app.route('/literals/:language').get(controller.list_literals);
   app.route('/emojis').get(controller.list_emojis);
+  app.route('/languageOptions').get(controller.language_options);
 };
--- src/language/languageProvider.tsx
+++ src/language/languageProvider.tsx
@@ -3,7 +3,7 @@ import React, { useState, useEffect } from 'react';
 import LanguageContext from './languageContext';
 import { getLiterals } from '../utils/request';
 
-import language from '../../public/literals/languageOptions.json';
+import language from '../../public/data/languageOptions.json';
 
 export default ({ children }) => {
   const [languageState, setLanguageState] = useState(language.options[0]);
