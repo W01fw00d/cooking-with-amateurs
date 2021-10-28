387284e83b0cb5f84383f5fc3ef11810db0821fd
Gabriel Romay Machado <romay.gabriel@gmail.com>
2021-10-28 18:48:31 +0900

    Update dev_tools/link.sh

--- dev_tools/link.sh
+++ dev_tools/link.sh
@@ -1,20 +1,10 @@
-cd ../
-echo "Building chemistry-ui"
-cd ../chemistry-ui
+echo "[...Starting cooking-with-amateurs/chemistry-ui Local Linking Script...]"
+cd ../../chemistry-ui
+echo "[...Linking cooking-with-amateurs/react and related libs to chemistry-ui...]"
+npm link ../cooking-with-amateurs/node_modules/react ../cooking-with-amateurs/node_modules/react-dom ../cooking-with-amateurs/node_modules/react-router-dom
+echo "[...Building chemistry-ui...]"
 npm run build
-echo "Linking react..."
-cd ../chemistry-ui/node_modules/react
-npm link
-cd ../../../cooking-with-amateurs
-npm link react
-echo "Linking react-router-dom..."
-cd ../chemistry-ui/node_modules/react-router-dom
-npm link
-cd ../../../cooking-with-amateurs
-npm link react-router-dom
-echo "Linking chemistry-ui..."
-cd ../chemistry-ui
-npm link
+echo "...[Linking chemistry-ui to cooking-with-amateurs]..."
 cd ../cooking-with-amateurs
-npm link chemistry-ui
-echo "Finished!"
+npm link ../chemistry-ui
+echo "[...Success!...]"
